0ec2f26227b4999530a307791da29b835ae28a5a
Thomas <jarriert@gmail.com>
2017-04-22 14:17:37 +0200

    Basic arborescence of my project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
+import { browserHistory } from 'react-router'
+import { syncHistoryWithSotre } from 'react-router-redux'
+import App from '@containers/App';
+import configureStore from '@store'
+import '@css/index.css';
 
-ReactDOM.render(
-  <App />,
+const store = configureStore()
+const history = syncHistoryWithSotre(browserHistory, store)
+
+render(
+  <App store={store} history={history} />,
   document.getElementById('root')
 );
