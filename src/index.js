62eafe7e17672abbc026af1814585b5dfeeb5ed9
Thomas <jarriert@gmail.com>
2017-04-24 13:25:14 +0200

    Introduce of bases is finish

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import configureStore from './store'
+import { configureStore, history } from './store'
 import { Provider } from 'react-redux'
-import { Router, browserHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
+import { Router } from 'react-router'
 import routes from './router'
 import './assets/css/index.css';
 
 const store = configureStore()
-const history = syncHistoryWithStore(browserHistory, store)
 
 const App = () => (
     <Provider store={store}>
