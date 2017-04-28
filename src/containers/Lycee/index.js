0fb4d4e0a92e263436882b89b5be682d827d9575
Thomas Jarrier <jarriert@gmail.com>
2017-04-28 17:04:51 +0200

    Save my work day

--- src/containers/Lycee/index.js
+++ src/containers/Lycee/index.js
@@ -10,12 +10,40 @@ class Lycee extends Component {
     render () {
         return (
             <div>
-                <p>
-                    Here, we are on the page contain the feature who will give the possibility of user to choose his Lycee.
-                </p>
+                <div className="jumbotron jumbotron-fluid">
+                    <div className="container">
+                        <h1 className="display-3">Informations</h1>
+                        <p className="lead">Here, we are on the page contain the feature who will give the possibility of user to choose his Lycee.</p>
+                    </div>
+                </div>
+                <form>
+                    <div className="form-group">
+                        <label for="exampleSelect1">Département</label>
+                        <select className="form-control" id="exampleSelect1">
+                            <option>1</option>
+                            <option>2</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleSelect1">Ville</label>
+                        <select className="form-control" id="exampleSelect1">
+                            <option>1</option>
+                            <option>2</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleSelect1">Lycée</label>
+                        <select className="form-control" id="exampleSelect1">
+                            <option>1</option>
+                            <option>2</option>
+                        </select>
+                    </div>
+                </form>
+                <button type="button" className="btn btn-success pull-2">Continuer</button>
             </div>
         )
     }
+
 
     static propTypes = {
         counter: PropTypes.number
